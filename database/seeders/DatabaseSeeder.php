a9f2fa7e3502e92a41618be79c52e56e01d9a180
unknown <you@example.com>
2022-02-07 13:56:26 +0100

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Filament\Facades\Filament;
+use Illuminate\Auth\EloquentUserProvider;
+use Illuminate\Auth\SessionGuard;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        /** @var SessionGuard $auth */
+        $auth = Filament::auth();
+
+        /** @var EloquentUserProvider $userProvider */
+        $userProvider = $auth->getProvider();
+
+        $userModel = $userProvider->getModel();
+
+        $user = $userModel::create([
+            'email'=>'admin@bfo.ch',
+            'name'=>'admin@bfo.ch',
+            'password'=>Hash::make('admin@bfo.ch'),
+        ]);
+    }
+}
