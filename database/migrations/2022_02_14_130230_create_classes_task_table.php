2555131680125e44afcba4033f8daa8d982e5be3
fateSimon <57984548+fateSimon@users.noreply.github.com>
2022-02-14 14:09:59 +0100

    created_migrations

---
+++ database/migrations/2022_02_14_130230_create_classes_task_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassesTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes_task', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('task_id');
+            $table->foreignId('class_id');
+            $table->foreignId('user_id');
+            $table->string('work_finished'); // TRUE if task done
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes_task');
+    }
+}
